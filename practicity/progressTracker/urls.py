0d6525145858ce7c6e26708d2f0d63ae244ad16b
Peter Hurt <peter.hurt@aol.de>
2020-09-19 11:14:46 +0200

    Added more views to the webserver app.

--- practicity/progressTracker/urls.py
+++ practicity/progressTracker/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /progressTracker
     path('', views.index, name='index'),
+    # ex:
+    path('session_detail/<int:practice_session_id>/', views.session_detail, name='session_detail'),
+    path('exercise_detail/<int:exercise_id>/', views.exercise_detail, name='exercise_detail'),
+    path('session_history/', views.session_history, name='session_history'),
 ]
--- practicity/progressTracker/views.py
+++ practicity/progressTracker/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World. This is the Progress Tracker App.")
+
+
+def session_detail(request, practice_session_id):
+    return HttpResponse("you are looking at the details of Practice Session id %s" % practice_session_id)
+
+
+def exercise_detail(request, exercise_id):
+    response = "You are looking at the details of the exercise %s "
+    return HttpResponse(response % exercise_id)
+
+
+def session_history(request):
+    response = "You can see your session history here"
+    return HttpResponse(response)
