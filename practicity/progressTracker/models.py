b599752a8533b97ccb2acc91a7126509f20a69b9
Peter Hurt <peter.hurt@aol.de>
2020-09-19 11:00:10 +0200

    improved models in app a little bit by functionality.

--- practicity/progressTracker/models.py
+++ practicity/progressTracker/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -7,8 +10,14 @@ class Exercise(models.Model):
     Exercise creates the table for single exercises.
     """
     exercise_name = models.CharField(max_length=200)
-    exercise_add_date = models.DateField('Date exercise added')
+    exercise_add_date = models.DateTimeField('Date exercise added')
     exercise_executed = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.exercise_name
+
+    def was_added_recently(self):
+        return self.exercise_add_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class PracticeSession(models.Model):
@@ -18,3 +27,6 @@ class PracticeSession(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     practice_session_start = models.DateTimeField('DateTime practice started')
     practice_session_end = models.DateTimeField('DateTime practice ended')
+
+    def __str__(self):
+        return "Practice session on: " + str(self.practice_session_start)
