53a89bf3385de0575f34bcdf538d5bf30f11d0da
Peter Hurt <peter.hurt@aol.de>
2020-09-27 10:37:14 +0200

    Styled the execution panel.

--- practicity/progressTracker/forms.py
+++ practicity/progressTracker/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Instrument
+from .models import Instrument, Exercise
 
 RATING_CHOICES =(
     ("1", "One"),
@@ -20,10 +20,10 @@ class ExecutionForm(forms.Form):
     ExecutionForm holds the fields of the exercise execution form which is needed
     on any template to track the execution of an exercise
     """
-    exercise = forms.CharField(label="Exercise", max_length=100)
+    exercise = forms.ModelChoiceField(Exercise.objects.all())
     instrument = forms.ModelChoiceField(Instrument.objects.all())
-    start = forms.CharField(label="Start time", max_length=100)
-    end = forms.CharField(label="End time", max_length=100)
+    start = forms.DateTimeField(label="Start time")
+    end = forms.DateTimeField(label="End time")
     rating = forms.ChoiceField(choices=RATING_CHOICES)
     tempo = forms.CharField(label="Tempo", max_length=100)
     timer = forms.CharField(label="Timer", disabled=True, required=False)
